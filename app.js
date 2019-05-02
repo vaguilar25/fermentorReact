fdbb173125c583373e196e080f99752e52e72819
vaguilar25 <vivian.aguilar@gmail.com>
2019-05-02 17:01:11 -0600

    change session to true

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(session({
   
   // Create API endpoints
   app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/', session: false }),
+    passport.authenticate('google', { failureRedirect: '/', session: true }),
     (req, res) => {
       console.log('wooo we authenticated, here is our user object:', req.user);
       res.json(req.user);
